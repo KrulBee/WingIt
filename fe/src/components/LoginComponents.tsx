5f103d0c747ffb782438424a0f12f84382be284b
KrulBee <galaxydragunov@gmail.com>
2025-06-15 03:22:10 +0700

    fe login font fix

--- fe/src/components/LoginComponents.tsx
+++ fe/src/components/LoginComponents.tsx
@@ -171,7 +171,7 @@ export const GhostButton = styled(Button)`
 
 export const Paragraph = styled.p`
   font-size: 14px;
-  font-weight: 100;
+  font-weight: 500;
   line-height: 20px;
   letter-spacing: 0.5px;
   margin: 20px 0 30px;
